a6e57089f8f8709006d35b89c7d04cd1bba39e02
Wesley Franks <code.wesleyfranks@gmail.com>
2021-06-01 13:57:44 -0500

    Adding work manager, adding remainder files

--- app/src/main/java/com/wesleyfranks/todo24/util/WorkManager.kt
+++ app/src/main/java/com/wesleyfranks/todo24/util/WorkManager.kt
@@ -1,4 +1,34 @@
 package com.wesleyfranks.todo24.util
 
-class WorkManager {
+import android.content.Context
+import android.util.Log
+import androidx.work.CoroutineWorker
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.invoke
+import kotlinx.coroutines.withContext
+
+class TaskTimeChecker(appContext: Context, workerParams: WorkerParameters):
+        CoroutineWorker(appContext, workerParams) {
+
+    companion object{
+        private const val TAG:String = "TaskTimeChecker"
+    }
+
+    override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
+
+        //checkTime()
+
+        return@withContext try {
+            // do something
+            Result.success()
+        } catch (error: Throwable){
+            Log.e(TAG, "doWork: $error")
+            Result.failure()
+        }
+
+    }
+
+
 }
